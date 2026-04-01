37a1acac8a4e2276c7aca7c511625b238222274a
muhamed-shahid <muhamedshahid7860@gmail.com>
2026-04-01 11:46:41 +0530

    job tracker day 2- Setting Auth Completed

--- server/server.js
+++ server/server.js
@@ -3,10 +3,13 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const app = express()
 require("dotenv").config()
+const authRoutes = require("./routes/authRoutes")
 
 
 app.use(express.json())
 app.use(cors())
+
+app.use("/api/auth",authRoutes)
 
 app.get("/",(req,res)=>{
     res.send("API is running")
