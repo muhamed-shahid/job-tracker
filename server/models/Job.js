c89b029678325e7289b53b99414983ec3024915c
muhamed-shahid <muhamedshahid7860@gmail.com>
2026-04-02 11:38:19 +0530

    job tracker day 3 - Setting auth middleware

---
+++ server/models/Job.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+
+const jobSchema = new mongoose.Schema({
+    title:String,
+    company:String,
+    location:String,
+    status:{
+        type:String,
+        enum:["APPLIED","INTERVIEW","OFFER","REJECTED"],
+        default:"APPLIED"
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+
+    }
+
+},{timestamps:true})
+
+
+module.exports = mongoose.model("Job",jobSchema)
