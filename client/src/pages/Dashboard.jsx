dea30834c9faef286b8298acfd63798f57b8d9b4
muhamed-shahid <muhamedshahid7860@gmail.com>
2026-04-08 10:39:49 +0530

    job tracker day 5 - Setting dashboard ui

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -18,7 +18,18 @@ const Dashboard = () => {
         fetchJobs()
     },[])
   return (
-    <h1>Dashboard</h1>
+    <div className="p-6">
+        <h1 className="text-2xl font-bold mb-4">My Jobs</h1>
+        <div className="grid gap-4">
+            {jobs.map((job)=>(
+                <div className="p-4 border rounded shadow" key={job._id}>
+                    <h2 className="font-bold">{job.company}</h2>
+                    <p>{job.position}</p>
+                    <p className="text-sm text-gray-500"></p>
+                </div>
+            ))}
+        </div>
+    </div>
   )
 }
 
